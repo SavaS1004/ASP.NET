29fbb4da48036c90c74a655884e2a57cc18d6aa3
Sava Alexandru <savaalex1004@yahoo.com>
2025-05-28 10:31:54 +0300

    The start of Model Biding section, i will work in the ModelBiding project

--- LearnMiddleware/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ LearnMiddleware/WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc57768baa571eeb06b37a22ece5113268df6a00")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
